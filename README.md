70bfbaa1d1b62981b1d9339fbe0daa01c6bdd4c1
wangshuo5 <wangshuo5@xiaomi.com>
2019-11-25 09:57:11 +0800

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+lerna-storybook-react
---
+++ src/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {withRouter} from 'react-router-dom'
+
+const Home = (props)=>{
+    console.log(props)
+    return(
+        <div>222</div>
+    )
+}
+export default withRouter(Home)
